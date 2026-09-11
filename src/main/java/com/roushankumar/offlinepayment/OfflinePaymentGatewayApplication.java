af4ab257292c041350e4dd0b10c125739f02e036
Roushankumar Mishra <mishraroushan455@gmail.com>
2026-09-11 19:54:42 +0530

    chore: initialize the Spring Boot Project

---
+++ src/main/java/com/roushankumar/offlinepayment/OfflinePaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.roushankumar.offlinepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflinePaymentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfflinePaymentGatewayApplication.class, args);
+    }
+
+}
